00dc1be638f377e79d1acf1d3c656b5e37706fd0
Iosif Karkanis <joskark@hotmail.com>
2024-11-01 10:30:06 +0100

    add css and content to blogs

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,17 +1,23 @@
 import { posts } from "../data";
-export default function Post({ params }: { params: { slug: string } }) {
-  // Find the post based on the slug
-  const post = posts.find((p) => p.slug === params.slug);
 
-  // If the post is not found, you can handle it gracefully
+export default async function Post({ params }: { params: { slug: string } }) {
+  // Extract the slug from params
+  const { slug } = await params;
+  const post = posts.find((p) => p.slug === slug);
+
+  // Handle case when post is not found
   if (!post) {
-    return <div>Post not found</div>;
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <p className="text-xl font-semibold text-red-500">Post not found</p>
+      </div>
+    );
   }
 
   return (
-    <div>
-      <h1>{post.title}</h1>
-      <p>{post.content}</p>
+    <div className="max-w-6xl mx-auto px-6 py-12 bg-white shadow-lg rounded-lg mt-10">
+      <h1 className="text-4xl font-bold text-gray-800 mb-6">{post.title}</h1>
+      <p className="text-gray-600 leading-relaxed text-lg">{post.content}</p>
     </div>
   );
 }
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,19 +1,30 @@
 import Link from "next/link";
 
 const posts = [
-  { slug: "post-1", title: "Post 1" },
-  { slug: "post-2", title: "Post 2" },
-  { slug: "post-3", title: "Post 3" },
+  {
+    slug: "post-1",
+    title: "Ρήξη Πρόσθιου Χιαστού Συνδέσμου (TPLO, CCWO, LSS)",
+  },
+  { slug: "post-2", title: "Κατάγματα Άκρων - Ενδοαρθρικά κατάγματα" },
+  { slug: "post-3", title: "Εξαρθρήματα Aρθρώσεων" },
+  { slug: "post-4", title: "Εξάρθρημα Επιγονατίδας" },
 ];
 
 export default function PostsList() {
   return (
-    <div>
-      <h1>Posts</h1>
-      <ul>
+    <div className="max-w-lg mx-auto mt-10 p-6 bg-white shadow-md rounded-lg">
+      <h1 className="text-3xl font-semibold text-gray-800 text-center mb-6">
+        Χρήσιμα άρθρα
+      </h1>
+      <ul className="space-y-4">
         {posts.map((post) => (
-          <li key={post.slug}>
-            <Link href={`/blog/${post.slug}`}>{post.title}</Link>
+          <li key={post.slug} className="text-center">
+            <Link
+              href={`/blog/${post.slug}`}
+              className="text-lg text-blue-600 font-medium hover:underline hover:text-blue-800 transition"
+            >
+              {post.title}
+            </Link>
           </li>
         ))}
       </ul>
