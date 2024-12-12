375d175e725ee6ad248490a897bb60b74af901b8
Iosif Karkanis <joskark@hotmail.com>
2024-12-12 21:46:30 +0200

    Add option for supporting trailing space to work with Cloudfront

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // npm run dev doesn't work with this enabled
+  /*
+  "trailingSlash" is needed with Cloudfront and S3 origin. CLoudfront is trying
+  to access the object directly in s3 but the file is saved as <name>.html
+  Since Cloudfront is trying to find the index.html object this solving the issue.
+  https://nextjs.org/docs/pages/api-reference/config/next-config-js/trailingSlash
+  */
+  trailingSlash: true,
+  output: "export",
   images: {
     unoptimized: true, // S3 doesn’t have server-side processing capabilities. This one disables next/Image processing which needs server-side capabilities
     remotePatterns: [
